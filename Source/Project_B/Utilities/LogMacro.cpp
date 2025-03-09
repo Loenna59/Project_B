cc893db81ed95753ea25790954973e3f4da4dbd5
heenchon <heenchon@naver.com>
2025-03-09 19:13:34 +0900

    chore: 로그매크로 함수

--- Source/Project_B/Utilities/LogMacro.cpp
+++ Source/Project_B/Utilities/LogMacro.cpp
@@ -1,7 +1,4 @@
-﻿#include "C:\Users\user\Desktop\PotenUp_1st_GameClient\Project_B\Intermediate\Build\Win64\x64\Project_BEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "LogMacro.h"
+﻿#include "LogMacro.h"
 #pragma once
-
-#include "LogMacro.h"
 
 DEFINE_LOG_CATEGORY(BAnimals);
