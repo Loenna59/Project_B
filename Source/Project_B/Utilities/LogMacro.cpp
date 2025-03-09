d63b661f17b9b07537a9955851d5ac516ad996db
heenchon <heenchon@naver.com>
2025-03-09 18:58:40 +0900

    add: 로그 출력 매크로 생성

---
+++ Source/Project_B/Utilities/LogMacro.cpp
@@ -0,0 +1,7 @@
+﻿#include "C:\Users\user\Desktop\PotenUp_1st_GameClient\Project_B\Intermediate\Build\Win64\x64\Project_BEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
+#include "LogMacro.h"
+#pragma once
+
+#include "LogMacro.h"
+
+DEFINE_LOG_CATEGORY(BAnimals);
---
+++ Source/Project_B/Utilities/LogMacro.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(BAnimals, Log, All);
+
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define PRINT_CALLINFO() \
+UE_LOG(BAnimals, Warning, TEXT("%s"), *CALLINFO)
+
+/**로그에 출력, LOG_PRINT(TEXT(" "))*/
+#define LOG_PRINT(Format, ...) \
+UE_LOG(BAnimals, Warning, Format, ##__VA_ARGS__);
+
+/**화면에 출력, 시간과 색상 설정 필요*/
+#define LOG_SCREEN_MY(Time, Color, Format, ...) \
+if (GEngine) GEngine->AddOnScreenDebugMessage(-1, Time, Color, FString::Printf(TEXT(Format), ##__VA_ARGS__));
+
+/**화면에 출력 LOG_SCREEN(" ")*/
+#define LOG_SCREEN(Format, ...) \
+LOG_SCREEN_MY(2.0f, FColor::Green, Format,  ##__VA_ARGS__)
+
+#define LOG_SCREEN_OBJ(Obj, Format, ...) \
+if (GEngine && Obj) \
+{ \
+FString Msg = FString::Printf(TEXT("[%s] " Format), *Obj->GetName(), ##__VA_ARGS__); \
+GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, Msg); \
+}
+
+/**화면과 로그 모두에 출력, LOG_ERROR(this, TEXT(" "))*/
+#define LOG_ERROR(Obj, Format, ...) { \
+const FString _ObjName = (Obj) ? (Obj)->GetName() : TEXT("NULL"); \
+UE_LOG(BAnimals, Error, TEXT("[%s] ") Format, *_ObjName, ##__VA_ARGS__); \
+if (GEngine) { \
+GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("[%s] ") Format, *_ObjName, ##__VA_ARGS__)); \
+} \
+}
