8ec82972a747ea2f123f32194260ab00c5e84782
heenchon <heenchon@naver.com>
2025-03-09 18:58:15 +0900

    add: 골대 액터 생성

---
+++ Source/Project_B/Maps/LuggageChaos/Luggage/LuggageHole.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LuggageHole.h"
+
+
+// Sets default values
+ALuggageHole::ALuggageHole()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ALuggageHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALuggageHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Project_B/Maps/LuggageChaos/Luggage/LuggageHole.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LuggageHole.generated.h"
+
+UCLASS()
+class PROJECT_B_API ALuggageHole : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALuggageHole();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
