3807dff1b2d2bd6a177d06397bfb2a75948ef2ab
heenchon <heenchon@naver.com>
2025-03-12 16:22:31 +0900

    feat: 골대 / 황금 러기지

---
+++ Source/Project_B/Maps/LuggageChaos/Luggage/GoldLuggage.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoldLuggage.h"
+
+
+// Sets default values
+AGoldLuggage::AGoldLuggage()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Point = 4;
+	Weight *= 2;
+}
+
+void AGoldLuggage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGoldLuggage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
