2571bda65cc55cccdfc12835cbf228ed18349241
heenchon <heenchon@naver.com>
2025-03-12 16:19:52 +0900

    add: Luggage spawnPoint 클래스

---
+++ Source/Project_B/Maps/LuggageChaos/Luggage/LuggageSpawnPoint.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Project_B/Maps/TeamMatchType.h"
+#include "LuggageSpawnPoint.generated.h"
+
+UCLASS()
+class PROJECT_B_API ALuggageSpawnPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALuggageSpawnPoint();
+
+	/**팀 스폰 위치라면, true로 변경*/
+	UPROPERTY(EditAnywhere)
+	ETeamType Team = ETeamType::Blue;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+};
