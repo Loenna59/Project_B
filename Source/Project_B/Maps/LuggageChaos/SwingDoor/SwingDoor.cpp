058dcc31c5937cfa7b90d4d3e336987fa98a2c72
heenchon <heenchon@naver.com>
2025-03-12 17:15:31 +0900

    chore: 러기지 게임모드, 캐릭터 설정

---
+++ Source/Project_B/Maps/LuggageChaos/SwingDoor/SwingDoor.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SwingDoor.h"
+
+
+ASwingDoor::ASwingDoor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASwingDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASwingDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
