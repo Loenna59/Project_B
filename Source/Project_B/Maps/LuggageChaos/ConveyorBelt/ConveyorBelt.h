3859f3d0d2f14e43ab59566d49ef8b311a95f8fb
heenchon <heenchon@naver.com>
2025-03-17 17:48:07 +0900

    feat: 컨베이어 벨트, 기본 동작

---
+++ Source/Project_B/Maps/LuggageChaos/ConveyorBelt/ConveyorBelt.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ConveyorBelt.generated.h"
+
+UCLASS()
+class PROJECT_B_API AConveyorBelt : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AConveyorBelt();
+	
+	UPROPERTY(EditAnywhere)
+	USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* EndBox;
+
+	UPROPERTY(EditAnywhere)
+	class UArrowComponent* StartArrow;
+	
+	UPROPERTY(EditAnywhere, Category = "Conveyor Belt")
+	TArray<UStaticMeshComponent*> Plates;
+
+	UPROPERTY(EditAnywhere, Category = "Conveyor Belt")
+	uint8 PlateCount = 16;
+
+	UPROPERTY(EditAnywhere, Category = "Conveyor Belt")
+	float Speed = 100.0f;
+
+	FVector MoveDir;
+
+protected:
+	virtual void BeginPlay() override;
+
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	void OnCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
