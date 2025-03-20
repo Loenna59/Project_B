ac4c0b3f993cda037ada0b006b9de0c93f35d9f6
heenchon <heenchon@naver.com>
2025-03-20 13:33:01 +0900

    chore: LuggageGameMode 정보, 게임 스테이트로 이전

---
+++ Source/Project_B/Maps/LuggageChaos/Game/LuggageChaosGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "LuggageChaosGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_B_API ALuggageChaosGameState : public AGameState
+{
+	GENERATED_BODY()
+};
