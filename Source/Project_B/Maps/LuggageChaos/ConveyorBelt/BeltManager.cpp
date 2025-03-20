2e3a592019cd6991f015af45c3183fef2b40bb54
heenchon <heenchon@naver.com>
2025-03-20 17:33:40 +0900

    test: 컨베이어 벨트 동기화 테스트

---
+++ Source/Project_B/Maps/LuggageChaos/ConveyorBelt/BeltManager.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BeltManager.h"
+
+
+// Sets default values
+ABeltManager::ABeltManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABeltManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABeltManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
