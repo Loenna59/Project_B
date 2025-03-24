bd08571e1db38acc5cbfe9312630562104aa6194
heenchon <heenchon@naver.com>
2025-03-24 22:34:43 +0900

    feat: 러기지 PlayerState 추가

---
+++ Source/Project_B/Maps/LuggageChaos/Game/LuggagePlayerState.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Project_B/Maps/TeamMatchType.h"
+#include "LuggagePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_B_API ALuggagePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	ETeamType MyTeamType = ETeamType::None;
+
+private:
+	bool bIsWin = false;
+	
+public:
+	void SetTeamType(const ETeamType team) { MyTeamType = team; }
+	ETeamType GetTeamType() const { return MyTeamType; }
+
+	void SetIsWin(bool isWin) { bIsWin = isWin; }
+};
